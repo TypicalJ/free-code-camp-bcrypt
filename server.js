6bfc6fe164f57cfda4050a348add34bd543a8f66
Justin Freeman <justinfreeman@Justins-iMac.attlocal.net>
2025-01-02 22:52:53 -0500

    add hash and hash compare with bcrypt

--- server.js
+++ server.js
@@ -11,8 +11,15 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+const hash = bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        /*res == true or false*/
+        console.log(res, " <= res")
+    });
+  });
 
-
+ 
 
 //END_ASYNC
 
